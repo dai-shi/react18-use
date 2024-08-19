f9fdc5744374350cbe5c5681c02523401cc18b5d
daishi <daishi@axlight.com>
2024-08-19 18:08:53 +0900

    refactor

---
+++ src/use.ts
@@ -0,0 +1,44 @@
+/// <reference types="react/experimental" />
+
+import ReactExports from 'react';
+
+type Usable<T> =
+  | (PromiseLike<T> & {
+      status?: 'pending' | 'fulfilled' | 'rejected';
+      value?: T;
+      reason?: unknown;
+    })
+  | React.Context<T>;
+
+function isContext<T>(usable: Usable<T>): usable is React.Context<T> {
+  return '_currentValue' in usable && '$$typeof' in usable;
+}
+
+export const use =
+  ReactExports.use ||
+  (<T>(usable: Usable<T>): T => {
+    if (isContext(usable)) {
+      // eslint-disable-next-line import/no-named-as-default-member
+      return ReactExports.useContext(usable);
+    }
+    if (usable.status === 'pending') {
+      throw usable;
+    } else if (usable.status === 'fulfilled') {
+      return usable.value as T;
+    } else if (usable.status === 'rejected') {
+      throw usable.reason;
+    } else {
+      usable.status = 'pending';
+      usable.then(
+        (v) => {
+          usable.status = 'fulfilled';
+          usable.value = v;
+        },
+        (e) => {
+          usable.status = 'rejected';
+          usable.reason = e;
+        },
+      );
+      throw usable;
+    }
+  });
