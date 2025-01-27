8e85c3266bd06e46eb9de4d599c428552a831602
daishi <daishi@axlight.com>
2025-01-27 11:57:35 +0900

    fix useMemo

--- src/memo.ts
+++ src/memo.ts
@@ -4,56 +4,66 @@ import { setOverride } from './use.js';
 import { getOriginalContext } from './context.js';
 import type { Store } from './context.js';
 
-const isEqualSet = <T>(a: Set<T>, b: Set<T>) =>
-  a.size === b.size && [...a].every((x) => b.has(x));
-
 // LIMITATION:
-// - Doesn't support deps
 // - Can tear?
 export const useMemo = <T>(fn: () => T, deps: readonly unknown[]) => {
-  type Subscribe = (cb: () => void) => () => void;
-  const cache = React.useMemo<{
-    subscribe: Subscribe;
-    storeSet: Set<Store<unknown>>;
-  }>(
-    () => ({ subscribe: () => () => {}, storeSet: new Set() }),
-    // eslint-disable-next-line react-compiler/react-compiler
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    deps,
-  );
-  return React.useMemo(
+  type Unsubscribe = () => void;
+  type Subscribe = (cb: () => void) => Unsubscribe;
+  type AddStore = (store: Store<unknown>) => void;
+  const cache = React.useMemo(
     () => {
+      const subscriptions = new Map<Store<unknown>, Unsubscribe>();
       const storeSet = new Set<Store<unknown>>();
-      try {
-        setOverride((usable) => {
-          const originalContext = getOriginalContext(usable);
-          if (!originalContext) {
-            throw new Error(
-              'use must be used with createContext from react18-use',
+      let callback: (() => void) | undefined;
+      const subscribe: Subscribe = (cb) => {
+        callback = cb;
+        for (const store of storeSet) {
+          if (!subscriptions.has(store)) {
+            subscriptions.set(
+              store,
+              store.subscribe(() => callback?.()),
             );
           }
-          const store = (React.use || React.useContext)(originalContext);
-          storeSet.add(store);
-          return store.getValue() as never;
-        });
-        const value = fn();
-        if (!isEqualSet(cache.storeSet, storeSet)) {
-          cache.subscribe = (cb) => {
-            const unsubs = new Set<() => void>();
-            storeSet.forEach((store) => unsubs.add(store.subscribe(cb)));
-            return () => unsubs.forEach((unsub) => unsub());
-          };
-          cache.storeSet = storeSet;
         }
-        // eslint-disable-next-line react-compiler/react-compiler
-        // eslint-disable-next-line react-hooks/rules-of-hooks
-        return React.useSyncExternalStore(cache.subscribe, () => value);
-      } finally {
-        setOverride(undefined);
-      }
+        return () => {
+          callback = undefined;
+          for (const unsub of subscriptions.values()) {
+            unsub();
+          }
+          subscriptions.clear();
+        };
+      };
+      const addStore: AddStore = (store) => {
+        storeSet.add(store);
+        if (callback) {
+          subscriptions.set(
+            store,
+            store.subscribe(() => callback?.()),
+          );
+        }
+      };
+      return { subscribe, addStore };
     },
     // eslint-disable-next-line react-compiler/react-compiler
     // eslint-disable-next-line react-hooks/exhaustive-deps
     deps,
   );
+  return React.useSyncExternalStore(cache.subscribe, () => {
+    try {
+      setOverride((usable) => {
+        const originalContext = getOriginalContext(usable);
+        if (!originalContext) {
+          throw new Error(
+            'use must be used with createContext from react18-use',
+          );
+        }
+        const store = (React.use || React.useContext)(originalContext);
+        cache.addStore(store);
+        return store.getValue() as never;
+      });
+      return fn();
+    } finally {
+      setOverride(undefined);
+    }
+  });
 };
